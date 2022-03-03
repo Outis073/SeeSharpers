932625ac03ba7de964e98853514fa9f250ea69fc
DvdMe <DvdMe@LAPTOP-FVRMHNC2>
2022-03-03 14:16:04 +0100

    Add project files.

---
+++ SeeSharpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeeSharpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
