0450b63b91c8dce87be33afad2baa76fc499b749
Glenn Meering <44021961+GekkeGlenn-Dev@users.noreply.github.com>
2022-03-10 16:38:24 +0100

    [FEATURE] Seat Allocation.  (#4)
    * Basis and dummydata for testing the automatic seat allocation service
    
    * Add id's to the seatRows and Seats
    
    * [TASK] Automatic-Seat-Allocation Updated old logic with new improved logic for automatic generating the seats for allocation them.
    
    * Make migration for the ViewingSeats
    
    * [TASK] Finish up SeatAllocateService.
    
    * [TASK] Add Unit tests for Seat class
    
    * [TASK] Commented out unit test, need to checkout with the team how to do this.

---
+++ SeeSharpersUnitTests/Services/SeatAllocateService/SeatAllocateServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SeeSharpers.Data;
+using SeeSharpers.Models;
+using SeeSharpers.Services.SeatAllocateService;
+
+
+namespace SeeSharpersUnitTest.Services.SeatAllocateService
+{
+    // TODO Check out how to mock and then finish up this tests.
+
+    // [TestClass]
+    public class SeatAllocateServiceTests
+    {
+        // private readonly Mock<ApplicationDbContext> _contentMock = new();
+        // private readonly Mock<Viewing> _viewingMock = new();
+        //
+        // [TestMethod]
+        // public void GetSeat_ThereIsASeatFoundWithXAndY_returnsSeat()
+        // {
+        //     // Arrange
+        //     var service = new SeeSharpers.Services.SeatAllocateService.SeatAllocateService(_viewingMock.Object, _contentMock.Object);
+        //
+        //     // Act
+        //     var result = service.GetSeat(-2, 0);
+        //
+        //     // Assert
+        //     Assert.IsNotNull(result);
+        //     Assert.IsInstanceOfType(result, typeof(Seat));
+        // }
+        //
+        // [TestMethod]
+        // public void GetSeat_ThereIsNoSeatFoundWithXAndY_returnsNull()
+        // {
+        // }
+        //
+        // [TestMethod]
+        // public void GetSeat_ThereIsASeatFoundWithPosition_returnsSeat()
+        // {
+        // }
+        //
+        // [TestMethod]
+        // public void GetSeat_ThereIsNoSeatFoundWithPosition_returnsNull()
+        // {
+        // }
+    }
+}
